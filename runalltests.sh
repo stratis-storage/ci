d3a293756c5e181ac5870b7779d7e32b6c057fb4
Bryan Gurney <bgurney@redhat.com>
2019-05-01 17:29:52 -0400

    ci: add runalltests.sh and project test scripts
    Add a test script that calls all tests in the continuous integration
    suite (as well as the test scripts themselves):
    
    - stratisd: stratisd
    -- (If no test_config.json file exists, "test-loop" is executed.)
    -- (If a test_config.json file exists, "test-real" is executed.)
    - cli.sh: stratis-cli
    - dm_test: devicemapper-rs
    
    The test installs all required packages (currently based on Fedora
    29), installs upstream rust, and creates a working directory called
    "workspace".
    
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

---
+++ runalltests.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# Run all tests from the Stratis CI test suite
+# Current distro: Fedora (29)
+
+PRESTAGE=`pwd`
+
+dnf -y install git \
+	dbus-devel \
+	gcc \
+	systemd-devel \
+	xfsprogs \
+	python3-tox \
+	python3-pytest \
+	dbus-python \
+	make \
+	device-mapper-persistent-data \
+	xfsprogs \
+	python3-hypothesis \
+	dbus-glib-devel \
+	python3-justbytes.noarch \
+	python3-setuptools \
+	dbus-python-devel \
+	python3-pyudev \
+	python3-wcwidth \
+	python3-pyparsing \
+	dbus-devel.x86_64 \
+	systemd-devel.x86_64 \
+	glibc-devel.x86_64 \
+	dbus-devel.i686 \
+	systemd-devel.i686 \
+	glibc-devel.i686 \
+	openssl-devel
+
+# The instructions for rustup say to run "curl https://sh.rustup.rs -sSf | sh".
+# The resulting script has an interactive prompt, which would hang.
+# Instead, download it to a shell script, and execute with the "-y" switch
+# to automatically install.
+curl -o install_rustup.sh https://sh.rustup.rs
+chmod +x install_rustup.sh
+./install_rustup.sh -y
+
+source $HOME/.cargo/env
+
+# For tests that use nbd, add the following line to udev:
+# (edit /usr/lib/udev/rules.d/60-block.rules to add nbd* to remove action)
+# ACTION!="remove", SUBSYSTEM=="block", KERNEL=="loop*|nvme*|sd*|vd*|xvd*|pmem*|mmcblk*|dasd*|nbd*", OPTIONS+="watch"
+
+rustup default 1.31.0
+rustup target add i686-unknown-linux-gnu
+
+# Then, choose the directory of the test to be executed, and prep
+# the $WORKSPACE environment variable.
+# cd stratisd
+# export WORKSPACE="/root/workspace/stratisd"
+
+mkdir workspace
+cd workspace
+
+if [ -s "$HOME/test_config.json" ]
+then
+	STRATISD_MODE="test-real"
+else
+	STRATISD_MODE="test-loop"
+fi
+echo "Executing stratisd test ($STRATISD_MODE)"
+git clone https://github.com/stratis-storage/stratisd.git
+cd stratisd
+export WORKSPACE=`pwd`
+$PRESTAGE/stratisd.sh $STRATISD_MODE
+RC_STRATISD=$?
+echo "Completed stratisd test ($STRATISD_MODE): status $RC_STRATISD"
+cd $PRESTAGE/workspace
+
+echo "Executing stratis-cli test..."
+git clone https://github.com/stratis-storage/stratis-cli
+cd stratis-cli
+export WORKSPACE=`pwd`
+$PRESTAGE/cli.sh
+RC_STRATISCLI=$?
+echo "Completed stratis-cli test: status $RC_STRATISCLI"
+cd $PRESTAGE/workspace
+
+echo "Executing devicemapper-rs test..."
+git clone https://github.com/stratis-storage/devicemapper-rs.git
+cd devicemapper-rs
+export WORKSPACE=`pwd`
+$PRESTAGE/dm_test.sh
+RC_DEVMAPPER=$?
+echo "Completed devicemapper-rs test: status $RC_DEVMAPPER"
+cd $PRESTAGE/workspace
+
+echo "End of prestage script."
+echo "Results:"
+echo "stratisd-$STRATISD_MODE: $RC_STRATISD"
+echo "stratis-cli: $RC_STRATISCLI"
+echo "devicemapper-rs: $RC_DEVMAPPER"
