efb4a00c32ffa31f704d52953f4533b3d85cbaab
Bryan Gurney <bgurney@redhat.com>
2019-05-02 14:50:44 -0400

    Checkout latest tag of dependencies
    For the tests, instead of only using the latest tag for
    dbus-client-gen and HEAD for dbus-signature-pyparsing,
    dbus-python-client-gen, and into-dbus-python, use a "for"
    loop that checks out the latest tag for all four.
    
    While here, also relocate the comment for setting the PYTHONPATH,
    moving it right above the relevant block.  Also, add newlines
    for clarity.
    
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- cli.sh
+++ cli.sh
@@ -40,18 +40,16 @@ fi
 cd $STRATIS_DEPS_DIR/stratisd
 make build
 
-# Set the PYTHONPATH to use the dependencies
-cd $STRATIS_DEPS_DIR/dbus-client-gen
-git fetch --tags
+for STRATIS_DEP in dbus-client-gen dbus-signature-pyparsing dbus-python-client-gen into-dbus-python
+do
+    cd $STRATIS_DEPS_DIR/$STRATIS_DEP
+    git fetch --tags
     LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
-echo "checking out $LATEST_TAG"
-git checkout $LATEST_TAG
-cd $STRATIS_DEPS_DIR/dbus-signature-pyparsing
-git pull origin master
-cd $STRATIS_DEPS_DIR/dbus-python-client-gen
-git pull origin master
-cd $STRATIS_DEPS_DIR/into-dbus-python
-git pull origin master
+    echo "checking out $STRATIS_DEP $LATEST_TAG"
+    git checkout $LATEST_TAG
+done
+
+# Set the PYTHONPATH to use the dependencies
 cd $WORKSPACE
 export PYTHONPATH=src:$STRATIS_DEPS_DIR/dbus-client-gen/src:$STRATIS_DEPS_DIR/dbus-python-client-gen/src:$STRATIS_DEPS_DIR/into-dbus-python/src:$STRATIS_DEPS_DIR/dbus-signature-pyparsing/src
 export STRATISD=$STRATIS_DEPS_DIR/stratisd/target/x86_64-unknown-linux-gnu/debug/stratisd
