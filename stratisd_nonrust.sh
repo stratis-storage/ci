8a3c75512e6bd6bff8aed743e626fca37ab00cc7
Bryan Gurney <bgurney@redhat.com>
2019-06-27 09:47:43 -0400

    ci: modify client-dbus dir check logic
    Check for the absence of the client-dbus directory, and exit early.
    This de-indents a large block of scripting.
    
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- stratisd_nonrust.sh
+++ stratisd_nonrust.sh
@@ -32,45 +32,46 @@ then
     rm -rf $STRATIS_DEPS_DIR
 fi
 
-if [ -d $WORKSPACE/tests/client-dbus ]
+if [ ! -d $WORKSPACE/tests/client-dbus ]
 then
-    echo "Running client-dbus tests"
-    export STRATISD=$WORKSPACE/target/debug/stratisd
-
-    if [ ! -f  /etc/dbus-1/system.d/stratisd.conf ]
-    then
-        cp $WORKSPACE/stratisd.conf /etc/dbus-1/system.d/
-    fi
-
-
-    if [ ! -x $STRATISD ]
-    then
-        echo "Required $STRATISD not found or not executable"
-        exit 1
-    fi
-
-    mkdir $STRATIS_DEPS_DIR
-    cd $STRATIS_DEPS_DIR
-
-    # Clone the python dependencies
-    git clone https://github.com/stratis-storage/dbus-python-client-gen.git
-    git clone https://github.com/stratis-storage/dbus-client-gen.git
-    git clone https://github.com/stratis-storage/into-dbus-python.git
-    git clone https://github.com/stratis-storage/dbus-signature-pyparsing.git
-
-    for STRATIS_DEP in dbus-client-gen dbus-signature-pyparsing dbus-python-client-gen into-dbus-python
-    do
-        cd $STRATIS_DEPS_DIR/$STRATIS_DEP
-        git fetch --tags
-        LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
-        echo "checking out $STRATIS_DEP $LATEST_TAG"
-        git checkout $LATEST_TAG
-    done
-    # Set the PYTHONPATH to use the dependencies
-    export PYTHONPATH=src:$STRATIS_DEPS_DIR/dbus-client-gen/src:$STRATIS_DEPS_DIR/dbus-python-client-gen/src:$STRATIS_DEPS_DIR/into-dbus-python/src:$STRATIS_DEPS_DIR/dbus-signature-pyparsing/src
-
-    cd $WORKSPACE/tests/client-dbus
-    make tests
-else
     echo "client-dbus directory does not exist: $WORKSPACE/tests/client-dbus"
+    exit 1
 fi
+
+echo "Running client-dbus tests"
+export STRATISD=$WORKSPACE/target/debug/stratisd
+
+if [ ! -f  /etc/dbus-1/system.d/stratisd.conf ]
+then
+    cp $WORKSPACE/stratisd.conf /etc/dbus-1/system.d/
+fi
+
+
+if [ ! -x $STRATISD ]
+then
+    echo "Required $STRATISD not found or not executable"
+    exit 1
+fi
+
+mkdir $STRATIS_DEPS_DIR
+cd $STRATIS_DEPS_DIR
+
+# Clone the python dependencies
+git clone https://github.com/stratis-storage/dbus-python-client-gen.git
+git clone https://github.com/stratis-storage/dbus-client-gen.git
+git clone https://github.com/stratis-storage/into-dbus-python.git
+git clone https://github.com/stratis-storage/dbus-signature-pyparsing.git
+
+for STRATIS_DEP in dbus-client-gen dbus-signature-pyparsing dbus-python-client-gen into-dbus-python
+do
+    cd $STRATIS_DEPS_DIR/$STRATIS_DEP
+    git fetch --tags
+    LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+    echo "checking out $STRATIS_DEP $LATEST_TAG"
+    git checkout $LATEST_TAG
+done
+# Set the PYTHONPATH to use the dependencies
+export PYTHONPATH=src:$STRATIS_DEPS_DIR/dbus-client-gen/src:$STRATIS_DEPS_DIR/dbus-python-client-gen/src:$STRATIS_DEPS_DIR/into-dbus-python/src:$STRATIS_DEPS_DIR/dbus-signature-pyparsing/src
+
+cd $WORKSPACE/tests/client-dbus
+make tests
