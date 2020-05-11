a1f5eea3da25927aff2f1be168df604cf0b1cda7
Bryan Gurney <bgurney@redhat.com>
2020-05-11 15:53:35 -0400

    ci: move Fedora dependency install to separate script
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

---
+++ dependencies_fedora.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+dnf -y install git \
+	make \
+	dbus-devel \
+	gcc \
+	systemd-devel \
+	xfsprogs \
+	python3-pytest \
+	dbus-python \
+	make \
+	device-mapper-persistent-data \
+	python3-coverage \
+	dbus-glib-devel \
+	python3-justbytes.noarch \
+	python3-setuptools \
+	dbus-python-devel \
+	python3-pyudev \
+	python3-dateutil \
+	python3-wcwidth \
+	python3-pyparsing \
+	python3-psutil \
+	dbus-devel.x86_64 \
+	systemd-devel.x86_64 \
+	glibc-devel.x86_64 \
+	dbus-devel.i686 \
+	systemd-devel.i686 \
+	glibc-devel.i686 \
+	openssl-devel \
+	clang \
+	llvm \
+	llvm-devel \
+	keyutils \
+	cryptsetup-devel
+
