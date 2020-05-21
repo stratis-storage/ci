f7b21d2da2540ec81ef886bc52785ced9f827a9e
Bryan Gurney <bgurney@redhat.com>
2020-05-21 14:52:42 -0400

    blackbox: move build and package to new script
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- blackbox/stratis-blackbox-run.sh
+++ blackbox/stratis-blackbox-run.sh
@@ -58,46 +58,10 @@ STRATIS_CLI_R=$(rpmspec -P stratis-cli.spec | grep ^Release | awk {'print $2'})
 STRATIS_CLI_RPMBASENAME="${STRATIS_CLI_N}-${STRATIS_CLI_V}-${STRATIS_CLI_R}"
 echo "stratis-cli package target: $STRATIS_CLI_RPMBASENAME"
 
-if [ -d output_rpms ]
-then
-	echo "output_rpms directory already exists.  Recreating..."
-	rm -rvf output_rpms
-fi
-mkdir output_rpms
-mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS}/
-
 ./reset-upstream-stratis-repos.sh
 
-
-# Build the stratisd package (along with the rust cargo vendor tarball)
-echo "Building stratisd test package..."
-
-# If the vendor directory exists, remove it.
-# The contents will be repopulated by "cargo vendor".
-if [ -d ${STRATISD_N}-${STRATISD_V}/vendor ]
-then
-	echo "stratisd vendor directory already exists.  Removing..."
-	rm -rf ${STRATISD_N}-${STRATISD_V}/vendor
-fi
-
-tar czvf ~/rpmbuild/SOURCES/${STRATISD_N}-${STRATISD_V}.tar.gz ${STRATISD_N}-${STRATISD_V}
-cd ${STRATISD_N}-${STRATISD_V}/
-cargo vendor && tar cJf ../${STRATISD_N}-${STRATISD_V}-vendor.tar.xz vendor/
-cd ..
-cp ${STRATISD_N}-${STRATISD_V}-vendor.tar.xz ~/rpmbuild/SOURCES
-echo "Executing rpmbuild for stratisd..."
-rpmbuild -bb stratisd.spec
-
-# Build the stratis-cli package
-echo "Building stratis-cli test package..."
-tar czvf ~/rpmbuild/SOURCES/${STRATIS_CLI_N}-${STRATIS_CLI_V}.tar.gz ${STRATIS_CLI_N}-${STRATIS_CLI_V}
-echo "Executing rpmbuild for stratis-cli..."
-rpmbuild -bb stratis-cli.spec
-
-# Find all of the stratis-related RPM files output via the rpmbuild
-# stage, (including debuginfo and debugsource), and copy them to the
-# staging directory.
-find ~/rpmbuild/RPMS/ -name stratis*.rpm -exec cp -v {} output_rpms/ \;
+echo "Starting build and package process..."
+./build-and-package.sh
 
 dnf -y install output_rpms/$STRATISD_RPMBASENAME*.rpm output_rpms/$STRATIS_CLI_RPMBASENAME*.rpm
 
