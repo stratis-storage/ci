e7d829652a48957fb867859dd743e6d395c273be
Bryan Gurney <bgurney@redhat.com>
2022-05-18 17:04:55 -0400

    _utils: use braces for empty dictionary
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- dependency_management/_utils.py
+++ dependency_management/_utils.py
@@ -72,7 +72,7 @@ def build_cargo_tree_dict(manifest_path):
     with subprocess.Popen(command, stdout=subprocess.PIPE) as proc:
         stream = proc.stdout
 
-        version_dict = dict()
+        version_dict = {}
         line = stream.readline()
         while line != b"":
             line_str = line.decode("utf-8").rstrip()
@@ -174,7 +174,7 @@ def build_cargo_metadata(manifest_path):
     package = packages[0]
     dependencies = package["dependencies"]
 
-    result = dict()
+    result = {}
     for item in dependencies:
         result[item["name"]] = SimpleSpec(item["req"])
 
