68df78a713c3efa09abb036a6215c58061c49a87
Bryan Gurney <bgurney@redhat.com>
2022-07-14 13:05:19 -0400

    Add testing generate_sourcerpms script
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

---
+++ mockbuild_test/generate_sourcerpms.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+set -e
+set -x
+
+if ! groups | grep mock; then
+	echo 'No mock group membership; be sure to install mock'
+	echo 'and add the current user to the "mock" group'
+	exit 1
+fi
+
+DATECODE=$(date +%Y%m%d)
+DIST_RELEASE=$1
+
+STRATISD_SPEC_VERSION=$(rpmspec -q --srpm --qf "%{version}\n" stratisd.spec)
+STRATISCLI_SPEC_VERSION=$(rpmspec -q --srpm --qf "%{version}\n" stratis-cli.spec)
+
+if [ -z $DIST_RELEASE ]; then
+	echo "Usage: $0 centos-stream | fedora-rawhide"
+	exit 1
+fi
+
+case $DIST_RELEASE in
+"centos-stream")
+	DIST="el9"
+	MOCKCONFIG="/etc/mock/centos-stream-9-x86_64.cfg"
+	;;
+
+"fedora-rawhide")
+	DIST="fc37"
+	MOCKCONFIG="/etc/mock/fedora-rawhide-x86_64.cfg"
+	;;
+
+"fedora-latest")
+	DIST="fc36"
+	MOCKCONFIG="/etc/mock/fedora-36-x86_64.cfg"
+	;;
+
+*)
+	echo "Usage: $0 centos-stream | fedora-rawhide | fedora-latest"
+	exit 1
+	;;
+esac
+
+if [ -d output ]; then
+	echo 'Clearing old output directory...'
+	rm -rf output
+fi
+
+for mockdir in SOURCES SPECS SRPMS RPMS; do
+	if [ -d $mockdir ]; then
+		echo 'Clearing old mock rpm directories...'
+		rm -rf $mockdir
+	fi
+done
+
+echo $DATECODE
+mkdir {SOURCES,SPECS,SRPMS,RPMS}
+mkdir {SRPMS,RPMS}/{stratisd,stratis-cli}
+mkdir output
+mkdir output/{stratisd,stratis-cli}
+
+cp stratisd.spec SPECS
+cp stratis-cli.spec SPECS
+
+if [ -d upstream ]; then
+	rm -rf upstream
+fi
+
+mkdir upstream
+cd upstream
+git clone https://github.com/stratis-storage/stratisd
+git clone https://github.com/stratis-storage/stratis-cli
+cd stratisd
+STRATISD_HEADREV=$(git rev-parse --short HEAD)
+echo $STRATISD_HEADREV
+../../../release_management/create_artifacts.py ../../SOURCES/ stratisd $STRATISD_SPEC_VERSION
+cd ..
+cd stratis-cli
+STRATISCLI_HEADREV=$(git rev-parse --short HEAD)
+echo $STRATISCLI_HEADREV
+../../../release_management/create_artifacts.py ../../SOURCES/ stratis-cli $STRATISCLI_SPEC_VERSION
+cd ../..
+
+mock --buildsrpm -r $MOCKCONFIG --spec SPECS/stratisd.spec --sources SOURCES/ --resultdir=SRPMS/stratisd/
+mock --buildsrpm -r $MOCKCONFIG --spec SPECS/stratis-cli.spec --sources SOURCES/ --resultdir=SRPMS/stratis-cli/
+
+for package in stratisd stratis-cli; do
+	find SRPMS -name "$package*.rpm" -exec cp -v -t output/$package {} +
+done
