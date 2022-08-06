fea43f2d91ccf7bf4df64cb284820aa5503b0c4c
mulhern <amulhern@redhat.com>
2022-08-05 21:03:14 -0400

    Minor updates
    Signed-off-by: mulhern <amulhern@redhat.com>

--- rust/set_lower_bounds/src/main.rs
+++ rust/set_lower_bounds/src/main.rs
@@ -5,8 +5,22 @@ fn main() {
         .features(CargoOpt::AllFeatures)
         .exec()
         .unwrap();
-    print!("Root package: {}\n", metadata.root_package().unwrap().name);
-    for package in metadata.packages {
-        print!("{}\n", package.name);
-    }
+
+    let resolve = metadata.resolve.as_ref().unwrap();
+
+    let root_package = metadata.root_package().unwrap();
+    let dependency_specs = &root_package.dependencies;
+
+    let tree_root_id = resolve.root.as_ref().unwrap();
+
+    assert!(&root_package.id == tree_root_id);
+
+    let root_node = resolve
+        .nodes
+        .iter()
+        .find(|n| &n.id == tree_root_id)
+        .unwrap();
+    let concrete_deps = &root_node.deps;
+
+    assert!(concrete_deps.len() == dependency_specs.len());
 }
