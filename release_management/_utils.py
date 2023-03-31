f47793c483629d8a8c4da6c64e121d1dd8534456
Bryan Gurney <bgurney@redhat.com>
2023-03-31 09:22:39 -0400

    Add timeout to requests.get() calls
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- release_management/_utils.py
+++ release_management/_utils.py
@@ -276,7 +276,7 @@ def get_changelog_url(repository_url, branch):
     :param str branch: the git branch
     """
     changelog_url = f"{repository_url}/blob/{branch}/CHANGES.txt"
-    requests_var = requests.get(changelog_url)
+    requests_var = requests.get(changelog_url, timeout=30)
     if requests_var.status_code != 200:
         raise RuntimeError("Page at URL {changelog_url} not found")
 
